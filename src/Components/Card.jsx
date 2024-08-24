aebb5ca466c8baef7f33635ef824994415f4335f
Ashton Belton <ashbelton24@gmail.com>
2024-08-24 17:39:34 -0600

    Changed a few files around and trouble solved the function of the game

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,48 +1,30 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import "../styles/Card.css";
 
 function Card({ card, handleClick }) {
     const [flipped, setFlipped] = useState(false);
 
     const handleCardClick = () => {
-        setFlipped(true);
+        setFlipped(true); 
         setTimeout(() => {
-            setFlipped(false);
-            handleClick(card.id);
-        }, 600);
-    };
-
+          setFlipped(false); 
+          handleClick(card.id); 
+        }, 800); 
+      };
+    
     return (
-        <div className="card" onClick={() => handleClick(card.id)}>
+        <div className={`card ${flipped ? "flipped" : ""}`} onClick={handleCardClick}>
           <div className="card-inner">
             <div className="card-front">
               <img src={card.image} alt={card.name} />
               <p>{card.name}</p>
             </div>
             <div className="card-back">
-              <img src="path/to/pokemon-card-back.jpg" alt="Card Back" />
+              <img src="/Images/pokemoncard.jpeg" alt="Back of Cards" />
             </div>
           </div>
         </div>
     );
-
-
-
-/* 
-    return (
-        <div className={`card ${flipped ? 'flip' : ''}`} onClick={handleCardClick}>
-            <div className="card-inner">
-                <div className="card-font">
-                    <img src="{card.image}" alt="{card.name}" />
-                    <p>{card.name}</p>
-                </div>
-                <div className="card-back">
-                    <img src="../images/pokemoncard.jpeg" alt="Card Back" />
-                </div>
-            </div>
-        </div>
-    );
-*/
 }
 
 export default Card;
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import Card from "./Card";
 import "../styles/GameBoard.css";
 
-function GameBoard({ cards, handleCardClick}) {
-    return (
-        <div className="game-board">
-            {cards.length > 0 ? (
-                cards.map(card => (
-                    <Card key={card.id} card={card} handleClick={handleCardClick} />
-                ))
-            ) : (
-                <p>Loading cards...</p>
-            )}
-        </div>
-    );
+function GameBoard({ cards, handleCardClick }) {
+  return (
+    <div className="game-board">
+      {cards.length > 0 ? (
+        cards.map((card) => (
+          <Card key={card.id} card={card} handleClick={handleCardClick} />
+        ))
+      ) : (
+        <p>Loading cards...</p>
+      )}
+    </div>
+  );
 }
 
 export default GameBoard;
