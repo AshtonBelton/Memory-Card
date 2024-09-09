cfccdd4e54c3d2f6b495fe743b22375852fa5097
Ashton Belton <ashbelton24@gmail.com>
2024-09-08 18:17:51 -0600

    Fixing the shuffling and flipping of the cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Card.css";
 
-function Card({ card, handleClick }) {
-    const [flipped, setFlipped] = useState(false);
-
+function Card({ card, handleClick, flipping }) {
     const handleCardClick = () => {
-        setFlipped(true); 
-        setTimeout(() => {
-          setFlipped(false); 
-          handleClick(card.id); 
-        }, 800); 
-      };
+        handleClick(card.id);
+    };
     
     return (
-        <div className={`card ${flipped ? "flipped" : ""}`} onClick={handleCardClick}>
-          <div className="card-inner">
-            <div className="card-front">
-              <img src={card.image} alt={card.name} />
-              <p>{card.name}</p>
+        <div className={`card ${flipping ? "flip-360" : ""}`} onClick={handleCardClick}>
+            <div className="card-inner">
+                <div className="card-front">
+                    <img src={card.image} alt={card.name} />
+                    <p>{card.name}</p>
+                </div>
+                <div className="card-back">
+                    <img src="/Images/pokemoncard.jpeg" alt="Back of Cards" />
+                </div>
             </div>
-            <div className="card-back">
-              <img src="/Images/pokemoncard.jpeg" alt="Back of Cards" />
-            </div>
-          </div>
         </div>
     );
 }
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import "../styles/GameBoard.css";
 
 function GameBoard({ cards, handleCardClick }) {
-  return (
-    <div className="game-board">
-      {cards.length > 0 ? (
-        cards.map((card) => (
-          <Card key={card.id} card={card} handleClick={handleCardClick} />
-        ))
-      ) : (
-        <p>Loading cards...</p>
-      )}
-    </div>
-  );
+	const [flipping, setFlipping] = useState(false);
+	const [shuffledCards, setShuffledCards] = useState(cards);
+
+	const shuffleArray = (array) => {
+		let shuffled = array.slice(); 
+		for (let i = shuffled.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+		}
+		return shuffled;
+	};
+
+	const handleShuffle = (cardId) => {
+		setFlipping(true); 
+		setTimeout(() => {
+			setShuffledCards(shuffleArray(cards));
+			handleCardClick(cardId); 
+			setFlipping(false); 
+		}, 1000); 
+	};
+
+
+	return (
+		<div className="game-board">
+		{cards.length > 0 ? (
+			cards.map((card) => (
+			<Card key={card.id} card={card} handleClick={handleShuffle} flipping={flipping} />
+			))
+		) : (
+			<p>Loading cards...</p>
+		)}
+		</div>
+	);
 }
 
 export default GameBoard;
